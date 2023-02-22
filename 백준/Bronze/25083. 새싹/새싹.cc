7583d001ae131563395decdbf4835d4a385840ba
genieu99 <74898231+genieu99@users.noreply.github.com>
2023-02-23 03:58:51 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printSpring() {
+    cout << "         ,r\'\"7" << endl;
+    cout << "r`-_   ,'  ,/" << endl;
+    cout << " \\. \". L_r'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |";
+}
+
+int main() {
+    printSpring();
+}
