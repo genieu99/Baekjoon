004a5757a272168e4ca53eded4a6e5de18aefcaf
genieu99 <74898231+genieu99@users.noreply.github.com>
2023-02-23 10:59:30 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string existedId;
+    string surprised = "\?\?!";
+
+    cin >> existedId;
+    cout << existedId << surprised << "\n";
+}
