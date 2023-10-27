c63a4af7ca2df88641ffb36ea741c826ce55631c
YUJIN MOON <74898231+genieu99@users.noreply.github.com>
2023-10-27 18:57:04 +0900

    [Refactor] 세탁소 사장 동혁.cc

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
@@ -4,11 +4,17 @@
 #include <algorithm>
 using namespace std;
 
+vector<int> coin;
+vector<int> coinNumber;
+void initialCoinVector();
 void calculateChange(int change);
+void printResult(int i);
 
 int main() {
     int testCase, change;
     cin >> testCase;
+
+    initialCoinVector();
 
     for (int i = 0; i < testCase; i++) {
         cin >> change;
@@ -16,17 +22,29 @@ int main() {
     }
 }
 
-void calculateChange(int change) {
+void initialCoinVector() {
     int quater = 25, dime = 10, nickel = 5, penny = 1;
-    int quaterNum, dimeNum, nickelNum, pennyNum;
-    int rest = change;
-    quaterNum = rest / quater;
-    rest = rest % quater;
-    dimeNum = rest / dime;
-    rest = rest % dime;
-    nickelNum = rest / nickel;
-    rest = rest % nickel;
-    pennyNum = rest / penny;
+    
+    coin.push_back(quater);
+    coin.push_back(dime);
+    coin.push_back(nickel);
+    coin.push_back(penny);
+}
 
-    cout << quaterNum << " " << dimeNum << " " << nickelNum << " " << pennyNum << endl;
+void calculateChange(int change) {
+    for (int i = 0; i < coin.size(); i++) {
+        coinNumber.push_back(change / coin[i]);
+        change = change % coin[i];
+        printResult(i);
+    }
+}
+
+void printResult(int i) {
+    if (i == coin.size() - 1) {
+        cout << coinNumber[i] << endl;
+        coinNumber.clear();
+    }
+    else {
+        cout << coinNumber[i] << " ";
+    }
 }
