be8faae45a2b4173d5062d03d80e804dad2f22a6
genieu99 <74898231+genieu99@users.noreply.github.com>
2023-12-19 19:19:50 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int width, length;
+    cin >> width >> length;
+
+    cout << width * length;
+}
