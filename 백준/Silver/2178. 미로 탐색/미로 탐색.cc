d3631f2e692e2eaea4dda2f049f5e626d04196cd
genieu99 <74898231+genieu99@users.noreply.github.com>
2024-02-01 15:00:49 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2248 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -4,7 +4,7 @@
 #define MAX 101
 using namespace std;
 
-int width, length;
+int row, column;
 int maze[MAX][MAX];
 bool visited[MAX][MAX];
 int dist[MAX][MAX];
@@ -19,19 +19,19 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> width >> length;
+    cin >> row >> column;
 
-    for (int i = 0; i < width; i++) {
-        string row;
-        cin >> row;
+    for (int i = 0; i < row; i++) {
+        string line;
+        cin >> line;
 
-        for (int j = 0; j < length; j++) {
-            maze[i][j] = row[j] - '0';
+        for (int j = 0; j < column; j++) {
+            maze[i][j] = line[j] - '0';
         }
     }
 
     BFS(0, 0);
-    cout << dist[width - 1][length - 1];
+    cout << dist[row - 1][column - 1];
 }
 
 void BFS(int start_x, int start_y) {
@@ -48,7 +48,7 @@ void BFS(int start_x, int start_y) {
             int x_new = x + x_dir[i];
             int y_new = y + y_dir[i];
 
-            if ((0 <= x_new && x_new < width) && (0 <= y_new && y_new < length) && !visited[x_new][y_new] && maze[x_new][y_new] == 1) {
+            if ((0 <= x_new && x_new < row) && (0 <= y_new && y_new < column) && !visited[x_new][y_new] && maze[x_new][y_new] == 1) {
                 visited[x_new][y_new] = true;
                 q.push(make_pair(x_new, y_new));
                 dist[x_new][y_new] = dist[x][y] + 1;
